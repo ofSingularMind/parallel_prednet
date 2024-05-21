c84746f834c1a6b36852a97b2d75dc389ff2f51b
Evalexii <a.h.ledbetter@student.tudelft.nl>
2024-05-21 13:03:12 +0200

    removed channel filtering from baseline

--- PPN_models/PPN_Baseline.py
+++ PPN_models/PPN_Baseline.py
@@ -211,28 +211,7 @@ class ParaPredNet(keras.Model):
                         BU_inp = None
                         TD_inp = None
                         layer([BU_inp, TD_inp, P_inp], direction="top_down", paddings=self.paddings[l])
-                        losing_ids = [  0,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,
-                                            14,  15,  16,  17,  19,  20,  21,  22,  23,  24,  25,  26,  27,
-                                            28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,
-                                            41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,
-                                            54,  55,  56,  57,  58,  59,  60,  61,  62,  63,  64,  65,  66,
-                                            67,  68,  69,  70,  71,  72,  73,  74,  75,  77,  79,  80,  81,
-                                            82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  93,  94,  95,
-                                            97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
-                                            110, 111, 112, 113, 114, 115, 116, 117, 118, 120, 121, 122, 123,
-                                            124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136,
-                                            137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149,
-                                            150, 151, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163,
-                                            164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 176, 177,
-                                            179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191]
-                        R = self.predlayers[l].states["R"]
-                        # set losing ids to 0 - Create a mask for the channels
-                        mask = tf.constant([i not in losing_ids for i in range(R.shape[-1])], dtype=tf.float32)
-                        # Reshape mask to be broadcastable across the batch and spatial dimensions
-                        mask = tf.reshape(mask, [1, 1, 1, R.shape[-1]])
-                        # Apply mask to the representation
-                        self.predlayers[l].states["R"] = R * mask
-                        
+
                     # Bottom and Middle layers
                     else:
                         BU_inp = None
